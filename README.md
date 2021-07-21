bc97b3b648a90d7b4206edf5260339922666f668
Alvaro Torres <atorr1020@gmail.com>
2021-07-20 21:04:30 -0500

    Instead of default browser alert make custom alert modals. #5, made customer alert overlay and box, added functions to make buttons work and prevent game running in background when 'dead'

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 ### Info
 Name: Alvaro Torres - @alvarotorrestx
 Changes: 
-- Created css, html and js folders to organize files in
-- Moved index.html into html folder
-- Moved script.js into js folder
-- Moved style.css into css folder
+- Created Alert Overlay
+- Created Alert Box
+- Changed Alert game score message
+- Added Play & Stop button on Alert
+- Created styling for alert box, overlay
+- Added pieces to script to display the alert
+ - Added conditional statements to prevent game continuing in background
+ - Added functions for buttons to work
+- Each piece added/changed has comments on what the purpose is/was for
+Apologies: I forgot to record pieces or entire pieces of me building the pieces, I did leave during the time of working on this, it slipped my mind.
 
 ### Issue
-#7 Create a complete folder structure for this project.
+Instead of default browser alert make custom alert modals. #5
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let character = document.getElementById('character');
 let block = document.getElementById('block');
+// Span to display Score #
+const score = document.getElementById('score');
+// ID to enable overlay of Alert
+const alert = document.getElementById('alertOverlay');
 
 let counter = 0;
 function jump(){
@@ -8,6 +12,11 @@ function jump(){
     setTimeout(function(){
         character.classList.remove('animate');
     }, 300);
+
+    // If alert overylay is shown, no longer allow jump in background
+    if (alert.style.display === 'flex') {
+        character.classList.remove('animate');
+    }
 }
 
 let checkDead = setInterval(function() {
@@ -17,12 +26,33 @@ let checkDead = setInterval(function() {
 
     if( blockLeft < 20 && blockLeft > -20 && characterTop>=130){
         block.style.animation = 'none';
-        alert("Game Over. Score: "+Math.floor(counter/100));
-        counter = 0;
-        block.style.animation = 'block 1s infinite linear';
+        // If dead, display alert overlay
+        alert.style.display = 'flex';
+        // Ensures to check if alert is shown
+        if (alert.style.display === 'flex') {
+            // Set game score display
+            score.innerHTML = Math.floor(counter/100);
+            // Stop counter under game and displays score #
+            document.getElementById('scoreSpan').innerHTML = Math.floor(counter/100);
+        }
     }
     else{
-        counter++;
-        document.getElementById('scoreSpan').innerHTML = Math.floor(counter/100);
+        // If alert overlay is shown run
+        if (alert.style.display === 'flex') {
+            // Sets counter #, stops
+            counter = counter;
+            // Stop counter under game and displays score #
+            document.getElementById('scoreSpan').innerHTML = Math.floor(counter/100);
+        } else {
+            counter++;
+            document.getElementById('scoreSpan').innerHTML = Math.floor(counter/100);
+        }
     }
 }, 10);
+
+// Function for stop button, gives thank message and offers to play again.
+function stop() {
+    document.getElementById('stop').style.display = 'none';
+    document.getElementById('gameScoreText').innerHTML = `Thanks for playing! 😎<br>If you change your mind, here's the play button!<br>👇`;
+    document.getElementById('continue').innerHTML = 'Play';
+}
